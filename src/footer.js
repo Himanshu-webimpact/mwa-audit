aa336ae68893ef50e964bb99f42af3ff399ce247
haw-rm <rm@webimpact.co.in>
2023-03-07 13:22:12 +0530

    mysiteauditor

--- src/footer.js
+++ src/footer.js
@@ -131,7 +131,7 @@ class Footer extends HTMLElement {
                 </h2>
                 <ul class="text-[#3d3d4d] dark:text-gray-400 text-xs">
                   <li class="mb-3">
-                    <a href="#" class="hover:underline"
+                    <a href="mysiteauditor-alternative.html" class="hover:underline"
                       >MySiteAuditor Alternative</a
                     >
                   </li>
